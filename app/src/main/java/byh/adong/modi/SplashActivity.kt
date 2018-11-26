c24c064a13eeab87c60f89084110bd83034c1f7d
YoungChan <byou0070@gmail.com>
2018-11-26 18:52:57 +0900

    first commit

---
+++ app/src/main/java/byh/adong/modi/SplashActivity.kt
@@ -0,0 +1,19 @@
+package byh.adong.modi
+
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class SplashActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
